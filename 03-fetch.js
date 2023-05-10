ebb9bf5ac5ac5216ac5ba6e8b56071501f4cd8f6
LizClaudia <20203tn128@utez.edu.mx>
2023-05-09 23:35:58 -0600

    Ya se muestra todo :)

--- 03-fetch.js
+++ 03-fetch.js
@@ -1,7 +1,9 @@
-const resp = fetch('https://reqres.in/api/users?page=1');
-resp.then((response)=>{
-  return response.json();
-}).then((json)=>{
+const resp = fetch("https://reqres.in/api/users?page=1");
+resp
+  .then((response) => {
+    return response.json();
+  })
+  .then((json) => {
     // let placeholder = document.getElementById("tr");
     // let odc= "";
     // for(let user of json.data){
@@ -11,64 +13,83 @@ resp.then((response)=>{
     //     <td>${user.email}</td>
     //   </tr>`
     // }
-    let htmlX =``;
-    let content = document.getElementById('container');
-    json.data.forEach(element => {
-        console.log(element)
-        
-        let htmlcard = ` 
+    let htmlX = ``;
+    let content = document.getElementById("container");
+    json.data.forEach((element) => {
+      console.log(element);
+
+      let htmlcard = ` 
         <div class="col-4">
-        <div class="card" id="cards" style="width: 18rem;">
+        <div class="card mb-3" id="cards " style="width: 18rem; ">
         <div class="card-body">
-        <div class="input-group mb-3">
-        <input type="text" class="form-control" value="${element.first_name}">
+        <div class=" mb-3">
+        <label for="name" class="col-form-label">Nombre</label>
+        <input type="text" id="name" class="form-control" value="${element.first_name}">
         </div>
-        <div class="input-group mb-3">
-        <input type="text" class="form-control" value="${element.last_name}">
+        <div class=" mb-3">
+        <label for="lastname" class=" col-form-label">Apellido Paterno</label>
+        <input type="text" id="lastname" class="form-control" value="${element.last_name}">
         </div>
-        <div class="input-group mb-3">
-        <input type="text" class="form-control" value="${element.email}">
+        <div class=" mb-3">
+        <label for="email" class=" col-form-label">Correo electrónico</label>
+        <input type="text" id="email" class="form-control" value="${element.email}">
         </div>
       
          <a href="#" class="btn btn-primary" onclick="showModal(${element.id})" value="">Ver detalles</a>
         </div>
-      </div> </div>`
-      htmlX = htmlX + htmlcard
+      </div> </div>`;
+      htmlX = htmlX + htmlcard;
     });
-    content.innerHTML=htmlX
-})
+    content.innerHTML = htmlX;
+  });
 
-function showModal(id){
-  console.log("Entro a la funci[on")
-  const myModal = new bootstrap.Modal('#showUser')
-  const resp = fetch(new URL(id,'https://reqres.in/api/users/'));
-  let htmlX =``;
-  let content1 = document.getElementById('Modalbody');
-  console.log(content1)
-  resp.then((response)=>{
+function showModal(id) {
+  console.log("Entro a la funci[on");
+  const myModal = new bootstrap.Modal("#showUser");
+  const resp = fetch(new URL(id, "https://reqres.in/api/users/"));
+  let htmlX = ``;
+  let content1 = document.getElementById("Modalbody");
+  console.log(content1);
+  resp
+    .then((response) => {
       return response.json();
-  }).then((json)=>{
-    console.log(json.data)
+    })
+    .then((json) => {
+      console.log(json.data);
       let htmlmodal = `
     <div class="modal-body">
-    <div class="col-auto">
+    <div class="col-12">
+    <div class="row">
+    <div class="col-md-4">
     <img  src="${json.data.avatar}">
     </div>
-      <div class="input-group mb-3">
-      <input type="text" class="form-control" value="${json.data.first_name}">
-      </div>
-      <div class="input-group mb-3">
-      <input type="text" class="form-control" value="${json.data.last_name}">
-      </div>
-      <div class="input-group mb-3">
+    <div class="col-md-8">
+    <div class=" mb-3">
+    <label for="name" class="col-form-label">Nombre</label>
+    <input type="text" class="form-control" value="${json.data.first_name}">
+    </div>
+    <div class="mb-3">
+    <label for="lastname" class="col-form-label">Apellido Paterno</label>
+    <input type="text" class="form-control" value="${json.data.last_name}">
+    </div>
+    <div class=" mb-3">
+    <label for="email" class" col-form-label">Correo electrónico</label>
       <input type="text" class="form-control" value="${json.data.email}">
       </div>
-      </div> `
-    htmlX = htmlX + htmlmodal
-    myModal.show();
-  })
-  content1.innerHTML=htmlX
-}
+    </div>
+    </div>
+    </div>
+    
+      
+      
+      </div> `;
+      console.log(htmlmodal);
 
+      htmlX = htmlX + htmlmodal;
+      console.log(htmlX);
+      myModal.show();
+      content1.innerHTML = htmlX;
+    });
+}
 
 //console.log(resp);
