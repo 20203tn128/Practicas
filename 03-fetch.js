7ecac1e42fb9a4752ed58825ae67c5a5144058dd
LizClaudia <20203tn128@utez.edu.mx>
2023-05-09 22:06:35 -0600

    Promesas con fetch 09/05/2023

--- 03-fetch.js
+++ 03-fetch.js
@@ -44,26 +44,18 @@ resp.then((response)=>{
 }).then((json)=>{
     let htmlX =``;
     let content = document.getElementById('container');
-    let htmlcard = ` 
-    <div class="col-4">
-    <div class="modal" tabindex="-1">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title">Modal title</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        <p>Modal body text goes here.</p>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
+    let htmlmodal = `<div class="modal-body">
+    <div class="input-group mb-3">
+    <input type="text" class="form-control" value="${element.first_name}">
     </div>
-  </div>
-</div> </div>`
-  htmlX = htmlX + htmlcard
+    <div class="input-group mb-3">
+    <input type="text" class="form-control" value="${element.last_name}">
+    </div>
+    <div class="input-group mb-3">
+    <input type="text" class="form-control" value="${element.email}">
+    </div>
+    </div> `
+  htmlX = htmlX + htmlmodal
 })
 content.innerHTML=htmlX
 //console.log(resp);
